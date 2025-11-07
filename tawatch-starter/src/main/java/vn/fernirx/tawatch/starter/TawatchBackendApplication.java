15a94684bdb5d9097cc121e85ce5aa32a0913ad7
Fernirx <anhtuhungdeveloper@gmail.com>
2025-11-07 17:56:42 +0700

    Initial project setup
    Set up Spring Boot multi-module Maven project structure with:
    - Parent POM configuration with Java 21 and Spring Boot 3.5.7
    - tawatch-starter module with basic application structure
    - Maven wrapper for consistent build environment
    - Standard .gitignore and .gitattributes files

---
+++ tawatch-starter/src/main/java/vn/fernirx/tawatch/starter/TawatchBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.fernirx.tawatch.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawatchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TawatchBackendApplication.class, args);
+    }
+
+}
---
+++ tawatch-starter/src/test/java/vn/fernirx/tawatch/starter/TawatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.fernirx.tawatch.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawatchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
