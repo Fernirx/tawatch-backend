228482301f9db7a41b351e970408e7c792b10ac2
Fernirx <anhtuhungdeveloper@gmail.com>
2025-12-07 17:13:34 +0700

    feat: configure Spring Boot application annotations
    - Set scanBasePackages to scan all "vn.fernirx.tawatch" packages
    - Enable JPA repositories scanning across all modules
    - Enable entity scanning across all modules
    - Enable configuration properties scanning
    - Enable async processing support

--- tawatch-starter/src/main/java/vn/fernirx/tawatch/starter/TawatchApplication.java
+++ tawatch-starter/src/main/java/vn/fernirx/tawatch/starter/TawatchApplication.java
@@ -2,8 +2,16 @@ package vn.fernirx.tawatch.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"vn.fernirx.tawatch"})
+@EnableJpaRepositories(basePackages = "vn.fernirx.tawatch")
+@EntityScan(basePackages = "vn.fernirx.tawatch")
+@ConfigurationPropertiesScan("vn.fernirx.tawatch")
+@EnableAsync
 public class TawatchApplication {
 
     public static void main(String[] args) {
