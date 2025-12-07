6079034500ebe5b5ada5844c38ca3db1cf6b75ba
Fernirx <anhtuhungdeveloper@gmail.com>
2025-12-07 18:31:23 +0700

    refactor: change JWT claim from username to email
    - Rename JWT_CLAIMS_USERNAME to JWT_CLAIMS_EMAIL in SecurityConstants
    - Reflect authentication using email as identifier
    - Maintain consistency with CustomUserDetails implementation

--- tawatch-common/src/main/java/vn/fernirx/tawatch/common/constant/SecurityConstants.java
+++ tawatch-common/src/main/java/vn/fernirx/tawatch/common/constant/SecurityConstants.java
@@ -11,7 +11,7 @@ public final class SecurityConstants {
     public static final String JWT_REFRESH_TOKEN = "refresh_token";
     public static final String JWT_RESET_PASSWORD_TOKEN = "reset_password_token";
     public static final String JWT_CLAIMS_TYPE = "type";
-    public static final String JWT_CLAIMS_USERNAME = "username";
+    public static final String JWT_CLAIMS_EMAIL = "email";
     public static final String JWT_CLAIMS_AUTHORITIES = "authorities";
 
     /* ================== HTTP HEADERS ================== */
